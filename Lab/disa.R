59c30b4f2c817a70c2edf6ccc34aa56d46754079
josephlara <joseph.lara@gmail.com>
2021-05-24 12:06:39 +0200

    stable vl, vls, tat

--- Lab/disa.R
+++ Lab/disa.R
@@ -15,14 +15,16 @@ library(purrr)
 
 rm(list = ls())
 
-#---- DEFINE MONTH AND PATHS -------------------------------------------------------
+#---- DEFINE PATHS AND VALUES THAT REQUIRE UPDATING EACH MONTH! -------------------------------------------------------
 
 file_monthly <- "data_source/monthly/Relatorio Mensal de Carga Viral (Abril).xlsx"
 month <- "2021-04-20"
 month_output <- "C:/Users/jlara/Documents/GitHub/Lab/Lab/data_source/monthly_processsed/2021_04.tsv"
+
+#---- DEFINE PATHS AND VALUES THE DO NOT REQUIRE UPDATING -------------------------------------------------------
+
 final_output <- "C:/Users/jlara/Documents/GitHub/Lab/Lab/output/disa.tsv"
-
-
+final_ajuda_output <- "C:/Users/jlara/Documents/GitHub/Lab/Lab/output/disa_ajuda.tsv"
 historic_files_path <- "C:/Users/jlara/Documents/GitHub/Lab/Lab/data_source/monthly_processsed/"  # PATH USED TO CREATE A LIST OF ALL .CSV FILES PREVIOUSLY CREATED
 compile_path <- "C:/Users/jlara/Documents/GitHub/Lab/Lab/data_source/monthly_processsed/"
 
@@ -58,11 +60,19 @@ xLW <- read_excel({file_monthly},
                 DISTRITO = DISTRICT) %>% 
   glimpse
 
-df <- dplyr::bind_rows(xAge, xSex, xPW, xLW)
+df_vl <- dplyr::bind_rows(xAge, xSex, xPW, xLW)
+rm(xAge, xSex, xPW, xLW)
 
-#---- PROCESS DATAFRAME -------------------------------------------------------
+df_tat <- read_excel("data_source/monthly/Relatorio Mensal de Carga Viral (Abril).xlsx", 
+                                                     sheet = "TRL", skip = 2)
 
-df_1 <- df %>% 
+ajuda_site_map <- read_excel("~/GitHub/AjudaSiteMap/AJUDA Site Map.xlsx")
+disa_site_map <- read_excel("~/GitHub/_GeneralJoins/DISA Datim Mapping.xlsx") %>% 
+  dplyr::select(Datim_ID, disa_site)
+
+#---- PROCESS VL DATAFRAME -------------------------------------------------------
+
+df_vl_1 <- df_vl %>% 
   dplyr::select(-c(`CV < 1000`, `CV > 1000`, TOTAL)) %>%
   dplyr::rename(province = PROVINCIA,
                 district = DISTRITO,
@@ -75,13 +85,14 @@ df_1 <- df %>%
                                           grepl("Repetir", indicator) ~ "Post Breastfeeding",
                                           grepl("Motivo de Teste n", indicator) ~ "Not Specified"),
                 result = dplyr::case_when(grepl("<1000", indicator) ~ "<1000",
-                                          grepl(">1000", indicator) ~ ">1000")) %>% 
+                                          grepl(">1000", indicator) ~ ">1000"),
+                tat_step = "temp") %>% 
   dplyr::select(-c(indicator)) %>% 
   glimpse()
 
-#---- RECODE AGE/SEX VALUES -----------------------------------------------
+#---- RECODE VL AGE/SEX VALUES -----------------------------------------------
 
-df_2 <- df_1 %>% 
+df_vl_2 <- df_vl_1 %>% 
   dplyr::mutate(age = dplyr::recode(age, "Idade não especificada" = "Unknown"),
                 age = dplyr::recode(age, "No Age Specified" = "Unknown"),
                 age = dplyr::recode(age, "Não especificada" = "Unknown"),
@@ -94,21 +105,40 @@ df_2 <- df_1 %>%
                 sex = dplyr::recode(sex, "M" = "Male"),
                 sex = tidyr::replace_na(sex, "Unknown"))
 
-#---- FILTER LINES ONLY >0 -----------------------------------------------
+#---- FILTER VL LINES ONLY >0 -----------------------------------------------
 
-df_3 <- df_2 %>% 
+df_vl_3 <- df_vl_2 %>% 
   dplyr::filter(value > 0) %>% 
   dplyr::mutate(indicator = "VL",
                 month = {month})
 
+#---- PROCESS TAT DATAFRAME -----------------------------------------------
+
+df_tat_1 <- df_tat %>% 
+  dplyr::select(9:15) %>%
+  dplyr::rename(province = PROVINCIA,
+                district = DISTRITO,
+                site = US) %>% 
+  tidyr::pivot_longer((`COLHEITA À RECEPÇÃO...12`:`ANÁLISE À VALIDAÇÃO...15`), names_to = "tat_step", values_to = "value") %>% 
+  dplyr::mutate(tat_step = dplyr::recode(tat_step, 
+                                         "COLHEITA À RECEPÇÃO...12" = "S1: Collection to Receipt",
+                                         "RECEPÇÃO AO REGISTO...13" = "S2: Receipt to Registration",
+                                         "REGISTO À ANÁLISE...14" = "S3: Registration to Analysis",
+                                         "ANÁLISE À VALIDAÇÃO...15" = "S4: Analysis to Validation"),
+                indicator = "TAT",
+                month = {month})
+
+df_final <- dplyr::bind_rows(df_vl_3, df_tat_1)
+
+
 #------ WRITE FILE TO DISK -------------------------------------------
 
 readr::write_tsv(
-  df_3,
+  df_final,
   {month_output},
   na ="")
 
-rm(df, df_1, df_2, df_3, xAge, xLW, xPW, xSex)
+rm(df_final, df_tat, df_tat_1, df_vl, df_vl_1, df_vl_2, df_vl_3)
 
 #---- DEFINE PATH AND SURVEY ALL MONTHLY TPT DATASETS THAT NEED TO BE COMBINED FOR HISTORIC DATASET ---------------------------------
 
@@ -118,7 +148,8 @@ historic_files <- dir({historic_files_path}, pattern = "*.tsv")  # PATH FOR PURR
 
 disa_vl <- historic_files %>%
   map(~ read_tsv(file.path(compile_path, .))) %>% 
-  reduce(rbind)
+  reduce(rbind) %>% 
+  glimpse()
 
 #---- SUBSET VLS DATASET AND MAKE INDEPENDENT INDICATOR -------------
 
@@ -129,16 +160,47 @@ disa_vls <- disa_vl %>%
 #---- UNION VL & VLS DATAFRAMES, PIVOT WIDER AND GROUP ----------------
 
 disa <- dplyr::bind_rows(disa_vl, disa_vls) %>% 
-  dplyr::mutate(row = row_number()) %>% 
+  dplyr::mutate(row = row_number(),
+                tat_step = na_if(tat_step, "temp")) %>% 
   tidyr::pivot_wider(names_from = indicator, values_from = value, values_fill = NULL) %>% 
-  dplyr::group_by(month, province, district, site, age, group, sex, motive) %>%
+  dplyr::group_by(month, province, district, site, age, group, sex, motive, tat_step) %>%
   summarise(VL = sum(VL, na.rm = T),
-            VLS = sum(VLS, na.rm = T)) %>%
-  ungroup()
+            VLS = sum(VLS, na.rm = T),
+            TAT = sum(TAT, na.rm = T)) %>%
+  ungroup() %>% 
+  glimpse()
 
+#---- CREATE AJUDA SUBSET OF DISA DATA -------------------------------
+
+disa_ajuda <- disa %>% 
+  dplyr::left_join(disa_site_map, c("site" = "disa_site")) %>% 
+  dplyr::left_join(ajuda_site_map, c("Datim_ID" = "orgunituid")) %>% 
+  tidyr::drop_na(Datim_ID) %>% 
+  dplyr::select(-c(SNU,
+                   Psnu,
+                   Sitename)) %>% 
+  dplyr::rename(orgunituid = Datim_ID,
+                partner = `IP FY20`,
+                lat = Lat,
+                long = Long) %>% 
+  dplyr::relocate(c(orgunituid, sisma_id, lat, long, partner), .after = site)
+  
 #------ WRITE FILE TO DISK -------------------------------------------
 
 readr::write_tsv(
   disa,
   {final_output},
   na ="")
+
+readr::write_tsv(
+  disa_ajuda,
+  {final_ajuda_output},
+  na ="")
+
+
+
+
+
+
+
+
--- Lab/disa_archive_processor.R
+++ Lab/disa_archive_processor.R
@@ -41,11 +41,15 @@ xLW <- read_excel({file_monthly},
   glimpse
 
 
-df <- dplyr::bind_rows(xAge, xSex, xPW, xLW)
+df_vl <- dplyr::bind_rows(xAge, xSex, xPW, xLW)
+rm(xAge, xSex, xPW, xLW)
+
+df_tat <- read_excel("data_source/monthly/Lab Monthly VL Archive.xlsx", 
+                                     sheet = "TRL")
 
 #---- MONTHLY RECODE -------------------------------------------------------
 
-df <- df %>%  
+df_vl_1 <- df_vl %>%  
   dplyr::mutate(Data = dplyr::recode(Data, 
                                      "January, 2020" = "2020-01-20",
                                      "February, 2020" = "2020-02-20",
@@ -65,9 +69,29 @@ df <- df %>%
                                      "April, 2021" = "2021-04-20")
                 )
 
+df_tat_1 <- df_tat %>%  
+  dplyr::mutate(Data = dplyr::recode(Data, 
+                                     "January, 2020" = "2020-01-20",
+                                     "February, 2020" = "2020-02-20",
+                                     "March, 2020" = "2020-03-20",
+                                     "April, 2020" = "2020-04-20",
+                                     "May, 2020" = "2020-05-20",
+                                     "June, 2020" = "2020-06-20",
+                                     "July, 2020" = "2020-07-20",
+                                     "August, 2020" = "2020-08-20",
+                                     "September, 2020" = "2020-09-20",
+                                     "October, 2020" = "2020-10-20",
+                                     "November, 2020" = "2020-11-20",
+                                     "December, 2020" = "2020-12-20",
+                                     "January, 2021" = "2021-01-20",
+                                     "February, 2021" = "2021-02-20",
+                                     "March, 2021" = "2021-03-20",
+                                     "April, 2021" = "2021-04-20")
+  )
+
 #---- PROCESS DATAFRAME -------------------------------------------------------
 
-df_1 <- df %>% 
+df_vl_1 <- df_vl_1 %>% 
   dplyr::select(-c(`CV < 1000`, `CV > 1000`, TOTAL)) %>%
   dplyr::rename(month = Data,
                 province = PROVINCIA,
@@ -81,14 +105,15 @@ df_1 <- df %>%
                                           grepl("Repetir", indicator) ~ "Post Breastfeeding",
                                           grepl("Motivo de Teste n", indicator) ~ "Not Specified"),
                 result = dplyr::case_when(grepl("<1000", indicator) ~ "<1000",
-                                          grepl(">1000", indicator) ~ ">1000")) %>% 
+                                          grepl(">1000", indicator) ~ ">1000"),
+                tat_step = "temp") %>% # THIS IS A PLACEHOLDER VALUE SO THAT  VARIABLE DOES NOT GET COERCED TO FACTOR WHEN BINDED TO HISTORIC DATA
   dplyr::select(-c(indicator)) %>% 
   glimpse()
 
 
 #---- RECODE AGE/SEX VALUES -----------------------------------------------
 
-df_2 <- df_1 %>% 
+df_vl_2 <- df_vl_1 %>% 
   dplyr::mutate(age = dplyr::recode(age, "Idade não especificada" = "Unknown"),
                 age = dplyr::recode(age, "No Age Specified" = "Unknown"),
                 age = dplyr::recode(age, "Não especificada" = "Unknown"),
@@ -103,13 +128,32 @@ df_2 <- df_1 %>%
 
 #---- FILTER LINES ONLY >0 -----------------------------------------------
 
-df_3 <- df_2 %>% 
+df_vl_3 <- df_vl_2 %>% 
   dplyr::filter(value > 0) %>% 
   dplyr::mutate(indicator = "VL")
+
+
+#---- PROCESS TAT DATAFRAME -----------------------------------------------
+
+df_tat_2 <- df_tat_1 %>% 
+  dplyr::rename(month = Data,
+                province = PROVINCIA,
+                district = DISTRITO,
+                site = US) %>% 
+  dplyr::select(-c(TOTAL)) %>%
+  tidyr::pivot_longer((`COLLECT TO RECEIVE`:`ANALYSIS TO VALIDATION`), names_to = "tat_step", values_to = "value") %>% 
+  dplyr::mutate(tat_step = dplyr::recode(tat_step, 
+                                         "COLLECT TO RECEIVE" = "S1: Collection to Receipt",
+                                         "RECEIVE TO REGISTRATION" = "S2: Receipt to Registration",
+                                         "REGISTRATION TO ANALYSIS" = "S3: Registration to Analysis",
+                                         "ANALYSIS TO VALIDATION" = "S4: Analysis to Validation"),
+                indicator = "TAT")
+
+df_final <- dplyr::bind_rows(df_vl_3, df_tat_2)
 
 #------ WRITE FILE TO DISK -------------------------------------------
 
 readr::write_tsv(
-  df_3,
+  df_final,
   {output},
   na ="")
